af9907fd0b050cf7815b433ce971f7ea7eda59e8
Thingkilia Finnatia Husin <vikasatibrahma@gmail.com>
2021-06-13 21:41:11 +0700

    updated

--- core/src/main/java/com/thing/bangkit/thingjetpackkotlin/core/helper/APIService.kt
+++ core/src/main/java/com/thing/bangkit/thingjetpackkotlin/core/helper/APIService.kt
@@ -1,6 +1,7 @@
 package com.thing.bangkit.thingjetpackkotlin.core.helper
 
 import com.thing.bangkit.thingjetpackkotlin.core.BuildConfig
+import com.thing.bangkit.thingjetpackkotlin.core.data.remote.FilmResponse
 import com.thing.bangkit.thingjetpackkotlin.core.data.remote.ValuesResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,8 +15,8 @@ interface APIService {
     suspend fun getTvShowList(): ValuesResponse
 
     @GET("movie/{id}?api_key=" + BuildConfig.API_KEY + "&language=en-US")
-    suspend fun getMovie(@Path("id") id: String): com.thing.bangkit.thingjetpackkotlin.core.data.remote.FilmResponse
+    suspend fun getMovie(@Path("id") id: String): FilmResponse
 
     @GET("tv/{id}?api_key=" + BuildConfig.API_KEY + "&language=en-US")
-    suspend fun getTvShow(@Path("id") id: String): com.thing.bangkit.thingjetpackkotlin.core.data.remote.FilmResponse
+    suspend fun getTvShow(@Path("id") id: String): FilmResponse
 }
